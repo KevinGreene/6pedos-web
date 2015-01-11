a712adc193140737abde5d645da14ad1c9232b9b
Roberto Guerra <uris77@gmail.com>
2015-01-11 10:23:41 -0600

    Updated gitignore

--- resources/public/js/out/reagent/debug.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 0.0-2657 {}
-if(!goog.isProvided_('reagent.debug')) {
-goog.provide('reagent.debug');
-}
-goog.require('cljs.core');
--- resources/public/js/out/reagent/interop.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 0.0-2657 {}
-if(!goog.isProvided_('reagent.interop')) {
-goog.provide('reagent.interop');
-}
-goog.require('cljs.core');
-reagent.interop.fvars = {};
