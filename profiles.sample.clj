1d40ea5a238f66ba474c9f410345542067c81250
Roberto Guerra <uris77@gmail.com>
2015-03-03 21:51:32 -0600

    Added sample project.clj

---
+++ profiles.sample.clj
@@ -0,0 +1,16 @@
+{:dev {:env
+       {:oauth-domain "http://localhost:3000"
+        :oauth-callback "/auth/callback"
+        :client-id "CLIENT_ID"
+        :client-secret "SECRET"
+        :auth-url "https://id.heroku.com/oauth/authorize"
+        :token-url "https://id.heroku.com/oauth/token"
+        :heroku-api-url "https://api.heroku.com/apps"
+        :csrf-token "acsrftoken"
+        :callback-url "http://localhost:3000/auth/callback"
+        :db-password  "password"
+        :db-name "sandbox"
+        :db-host "localhost"
+        :db-port 45970
+        :db-user "user"}}}
+
